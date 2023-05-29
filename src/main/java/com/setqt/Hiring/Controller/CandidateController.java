0998b3f8905401a4ce7a69b6ea7a7cdb589254fc
Triệu Quốc Thái <75628098+SETQT@users.noreply.github.com>
2023-05-29 23:29:31 +0700

    Update CandidateController.java

--- src/main/java/com/setqt/Hiring/Controller/CandidateController.java
+++ src/main/java/com/setqt/Hiring/Controller/CandidateController.java
@@ -26,6 +26,7 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/candidate")
+@CrossOrigin(origins = "*")
 public class CandidateController {
     @Autowired
     JwtTokenHelper jwtHelper;
