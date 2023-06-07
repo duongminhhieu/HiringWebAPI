6f165904d0797092d02b0ad270e55264e6af6d34
20120473 <76527212+duongminhhieu@users.noreply.github.com>
2023-06-07 12:00:03 +0700

    a

--- src/main/java/com/setqt/Hiring/HiringApplication.java
+++ src/main/java/com/setqt/Hiring/HiringApplication.java
@@ -2,12 +2,14 @@ package com.setqt.Hiring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HiringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HiringApplication.class, args);
 	}
-
 }
