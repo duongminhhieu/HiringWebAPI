c706888ba3798886150e060aaab294f79b90ee63
SETQT <tqthai1852@gmail.com>
2023-06-08 16:39:11 +0700

    redeploy

--- src/main/java/com/setqt/Hiring/Service/JobDescription/IJobDescription.java
+++ src/main/java/com/setqt/Hiring/Service/JobDescription/IJobDescription.java
@@ -3,6 +3,7 @@ package com.setqt.Hiring.Service.JobDescription;
 import com.setqt.Hiring.Service.Generic.IGenericService;
 import com.setqt.Hiring.Model.JobDescription;
 
+
 public interface IJobDescription extends IGenericService<JobDescription> {
 
 }
