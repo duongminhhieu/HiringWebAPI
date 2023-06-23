dcd10d5f1caf6cd1b7df2a9ddd54f7d23269aff6
Triệu Quốc Thái <75628098+SETQT@users.noreply.github.com>
2023-06-23 21:23:57 +0700

    Update WebSocketConfig.java

--- src/main/java/com/setqt/Hiring/WebSocket/WebSocketConfig.java
+++ src/main/java/com/setqt/Hiring/WebSocket/WebSocketConfig.java
@@ -15,6 +15,6 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(notificationWebSocketHandler, "/notification").setAllowedOrigins("*");
+        registry.addHandler(notificationWebSocketHandler, "/notify").setAllowedOrigins("*");
     }
 }
